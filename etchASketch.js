68e2df454feb14f65d76a1821327efc6a93bc963
Thomas <osucollab@gmail.com>
2023-09-26 20:44:19 -0400

    adds several features to the page as described:
    added custom fonts for the header, buttons, and range slider.
    added adjustable display grid intended for drawing; adjusted
    via. a range slider from 1 x 1 to 100 x 100 grid.
    added main layout for the page.
    added configuration buttons for colors and drawing conveniences

--- etchASketch.js
+++ etchASketch.js
@@ -1,9 +1,33 @@
 const DEFAULT_COLUMN_SIZE = 16;
-document.addEventListener("DOMContentLoaded", initializeGrid(DEFAULT_COLUMN_SIZE));
+document.addEventListener("DOMContentLoaded", setGrid(DEFAULT_COLUMN_SIZE));
 
-function initializeGrid(columnSize)
+let cellRangeSlider = document.querySelector('.cell-slider');
+
+cellRangeSlider.addEventListener('input', function()
 {
+    setGrid(cellRangeSlider.value);
+});
+
+function setGrid(columnSize)
+{
+    let cellSliderText = document.querySelector('.slider-text');
+    cellSliderText.textContent = columnSize.toString() + " x " + columnSize.toString();    
+
     const gridContainer = document.querySelector('#grid-container');
+
+    // Need to clear the grid items (children of the container) before adding new cells, if they exist
+            // #grid-container has at least a child: .grid-column if entering the loop:
+    while (gridContainer.firstChild)
+    {
+        // .grid-column, a child of #grid-container, contains at least a child: .cell if entering the loop:
+        while(gridContainer.firstChild.firstChild)
+        {
+            // remove the cell child from the column
+            gridContainer.firstChild.removeChild(gridContainer.firstChild.firstChild);
+        }
+        // remove the column child from the container
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
 
     for (let i = 0; i < columnSize; i++)
     {
@@ -15,15 +39,27 @@ function initializeGrid(columnSize)
             const cell = document.createElement('div');
             cell.classList.add('grid-cell');
             column.appendChild(cell);
-
+            cell.style.backgroundColor = "rgb(200, 200, 200)";
             cell.addEventListener("mouseover", function ()
             { hoveredCell(cell) });
+            cell.addEventListener("mouseout", function ()
+            { stoppedHoveredCell(cell) });
         }
         gridContainer.appendChild(column);
     }
 }
 
+function resetCells()
+{
+
+}
+
 function hoveredCell(cell)
 {
     cell.style.backgroundColor = "red";
+}
+
+function stoppedHoveredCell(cell)
+{
+    cell.style.backgroundColor = "rgb(200, 200, 200)";
 }
